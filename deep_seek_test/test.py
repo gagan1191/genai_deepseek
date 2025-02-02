3e8ba00f9f8d0fc9a2f54a5a7c7a840b45bb4d1b
gagan1191 <gagan6011@gmail.com>
2025-02-02 14:29:23 -0800

    first change

---
+++ deep_seek_test/test.py
@@ -0,0 +1,8 @@
+from agno.agent import Agent, RunResponse
+from agno.models.deepseek import DeepSeek
+from typing import List
+
+#agent = Agent(model=DeepSeek(), markdown=True)
+
+# Print the response in the terminal
+#agent.print_response("Share a 2 sentence horror story.")
